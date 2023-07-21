151c91dba6fdfeb5f1cce9e9e5e40f1a5089e71b
Mauricio <mauriciosundejimenez@gmail.com>
2023-07-20 23:28:32 -0400

    fixedSolutionFor242

--- 242.cs
+++ 242.cs
@@ -5,12 +5,13 @@ public class Solution {
         Array.Sort(lettersOf_S);
         char[] lettersOf_T = t.ToCharArray();
         Array.Sort(lettersOf_T);
-        return IsAnagramVerified(lettersOf_S,lettersOf_T);
+
+        for (int i = 0; i < lettersOf_S.Length; i++){
+            if (lettersOf_S[i] != lettersOf_T[i]){
+                return false;
+            }
+        }
+        return true;
     }
 
-    private bool IsAnagramVerified(char[] s, char[] t, int i = 0, int j = 0){
-        if (i >= s.Length) return true;
-        if (s[i] == t[j]) return IsAnagramVerified(s,t,i + 1, j + 1);
-        else return false;
-    }
 }
