629b957a0a7e09b51a1d5f851a294bc5f3f7cd3c
Mauricio <mauriciosundejimenez@gmail.com>
2023-07-20 23:37:58 -0400

    HasAnotherSolution

--- 242.cs
+++ 242.cs
@@ -1,13 +1,31 @@
-public class Solution {    
-    public bool IsAnagram (string s, string t){
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
         if (s.Length != t.Length) return false;
         char[] lettersOf_S = s.ToCharArray();
         Array.Sort(lettersOf_S);
         char[] lettersOf_T = t.ToCharArray();
         Array.Sort(lettersOf_T);
 
-        for (int i = 0; i < lettersOf_S.Length; i++){
-            if (lettersOf_S[i] != lettersOf_T[i]){
+        for (int i = 0; i < lettersOf_S.Length; i++)
+        {
+            if (lettersOf_S[i] != lettersOf_T[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public bool IsAnagram(string s, string t)//Another solution
+    {
+        if (s.Length != t.Length) return false;
+
+        var hash_S = s.ToHashSet();
+
+        foreach (var i in t){
+            if (!hash_S.Contains(i)){
                 return false;
             }
         }
