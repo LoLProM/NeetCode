b0a504e0a8a42b7f9783b21bfebb164f79556c88
Mauricio <mauriciosundejimenez@gmail.com>
2023-07-21 00:06:01 -0400

    second solution

--- 242.cs
+++ 242.cs
@@ -1,23 +1,5 @@
 public class Solution
 {
-    public bool IsAnagram(string s, string t)
-    {
-        if (s.Length != t.Length) return false;
-        char[] lettersOf_S = s.ToCharArray();
-        Array.Sort(lettersOf_S);
-        char[] lettersOf_T = t.ToCharArray();
-        Array.Sort(lettersOf_T);
-
-        for (int i = 0; i < lettersOf_S.Length; i++)
-        {
-            if (lettersOf_S[i] != lettersOf_T[i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public bool IsAnagram(string s, string t)//Another solution
     {
         if (s.Length != t.Length) return false;
