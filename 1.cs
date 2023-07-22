a4401ed56bc50a79364d2d23b61200c8b572a9cb
Mauricio <mauriciosundejimenez@gmail.com>
2023-07-22 17:49:36 -0400

    I Fixed the solution for 1-TwoSums

--- 1.cs
+++ 1.cs
@@ -1,13 +1,15 @@
 public class Solution
 {
     public int[] TwoSum(int[] nums, int target){
-        Array.Sort(nums);
+        int[] indexArray = Enumerable.Range(0, nums.Length).ToArray();
+        Array.Sort(nums, indexArray);
+
         int left = 0;
         int right = nums.Length - 1;
         while (left < right){
             int sum = nums[left] + nums[right];
             if (sum == target){
-                return new int[] { left, right };
+                return new int[] { indexArray[left], indexArray[right] };
             }
             if (sum > target){
                 right--;
