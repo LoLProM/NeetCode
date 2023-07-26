c72b68dcbbc93385e48ee93101ce3da109eea07c
Mauricio <mauriciosundejimenez@gmail.com>
2023-07-26 16:16:58 -0400

    Add 217.cs

---
+++ 217.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+
+        Dictionary<int, bool> listOfNumbers = new Dictionary<int, bool>();
+        foreach (var item in nums)
+        {
+            if (listOfNumbers.ContainsKey(item))
+            {
+                return true;
+            }
+            listOfNumbers.Add(item, true);
+        }
+        return false;
+    }
+}
