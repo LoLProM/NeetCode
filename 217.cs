4c33dfc21c2ae391e86d465d25d1a6d2ae33ec29
Andy Guerra <andyaguerra91@gmail.com>
2023-07-26 16:25:32 -0400

    Revert 217.cs to original state

--- 217.cs
+++ 217.cs
@@ -1,13 +1,9 @@
-public class Solution
-{
-    public bool ContainsDuplicate(int[] nums)
-    {
-
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        
         Dictionary<int, bool> listOfNumbers = new Dictionary<int, bool>();
-        foreach (var item in nums)
-        {
-            if (listOfNumbers.ContainsKey(item))
-            {
+        foreach (var item in nums){
+            if (listOfNumbers.ContainsKey(item)){
                 return true;
             }
             listOfNumbers.Add(item, true);
